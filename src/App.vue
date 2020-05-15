fb73fb933ba2f35b0a05cc4ab0f877cb53437c21
Delissa <delissacbeaman@gmail.com>
2020-05-15 17:42:57 -0500

    Just installed Big Picture theme for the second time

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,77 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <!-- <router-link to="/about">About</router-link>| -->
-      <router-link to="/signup">Signup</router-link>|
-      <router-link to="/login">Login</router-link>|
-      <router-link to="/styles">Styles</router-link>|
-      <router-link to="/locations">Locations</router-link>|
-      <router-link to="/requests">Saved Requests</router-link>|
-      <router-link to="/logout">Logout</router-link>|
-    </div>
     <router-view />
+    <!-- Header -->
+    <header id="header">
+      <h1>Roam Around</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/signup">Signup</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="/styles">Trip Styles</a>
+          </li>
+          <li>
+            <a href="/locations">Locations</a>
+          </li>
+          <li>
+            <a href="/logout">Logout</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Footer -->
+    <footer id="footer">
+      <!-- Icons -->
+      <ul class="icons">
+        <li>
+          <a href="#" class="icon brands fa-twitter">
+            <span class="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-facebook-f">
+            <span class="label">Facebook</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-instagram">
+            <span class="label">Instagram</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-linkedin-in">
+            <span class="label">LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-dribbble">
+            <span class="label">Dribbble</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="icon brands fa-pinterest">
+            <span class="label">Pinterest</span>
+          </a>
+        </li>
+      </ul>
+
+      <!-- Menu -->
+      <ul class="menu">
+        <li>&copy; Untitled</li>
+        <li>
+          Design:
+          <a href="/https://html5up.net">HTML5 UP</a>
+        </li>
+      </ul>
+    </footer>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #b942b9;
-}
 </style>
