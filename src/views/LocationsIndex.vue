a55645911cb5dde5947aed87d480d6a0fb2b2cca
Delissa <delissacbeaman@gmail.com>
2020-05-17 18:12:35 -0500

    Pictures on frontend work without error

--- src/views/LocationsIndex.vue
+++ src/views/LocationsIndex.vue
@@ -4,6 +4,18 @@
       <h3>{{ location.name }}</h3>
       <p>Intro: {{ location.intro }}</p>
       <p>Sightseeing Score: {{ location.sightseeing_score}}</p>
+      <p>Images: {{ location.images }}</p>
+      <img v-bind:src="location.images" v-bind:alt="location.images" />
+      <p>Country: {{ location.country_id }}</p>
+      <!-- <p>Score: {{ location.score }}</p>
+      <p>Climate: {{ location.climate }}</p>
+      <p>Beach Score: {{ location.district_beach_score }}</p>
+      <p>Dancing Score: {{ location.dancing_score }}</p>
+      <p>Night Life Score: {{ location.nightlife_score }}</p>
+      <p>Kid Friendly Score: {{ location.character-Kid_friendly_score }}</p>-->
+
+      <!-- t.integer "Location_id" -->
+
       <hr />
     </div>
   </div>
